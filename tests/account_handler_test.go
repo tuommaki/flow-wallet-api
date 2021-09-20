1600d9d04fec52defdaf9b1e8a21c1852cba49de
Tuomas Mäkinen <tuomas@giantswarm.io>
2021-09-20 12:54:02 +0300

    Support deposit events for non-custodial accounts
    Add support to add and remove non-custodial accounts that can be watched
    for deposit events.
    
    This change adds a new column `Type` to `Account` to distinct `custodial` and
    `non-custodial` accounts.

--- tests/account_handler_test.go
+++ tests/account_handler_test.go
@@ -93,6 +93,48 @@ func TestEmulatorAcceptsSignedTransaction(t *testing.T) {
 	}
 }
 
+func TestWatchlistAccountManagement(t *testing.T) {
+	cfg := test.LoadConfig(t, testConfigPath)
+	fc := test.NewFlowClient(t, cfg)
+	svcs := test.GetServices(t, cfg)
+	km := svcs.GetKeyManager()
+
+	accHandler := handlers.NewAccounts(test.Logger(), svcs.GetAccounts())
+
+	router := mux.NewRouter()
+	router.Handle("/", accHandler.AddNonCustodialAccount()).Methods(http.MethodPost)
+	router.Handle("/{address}", accHandler.Details()).Methods(http.MethodGet)
+	router.Handle("/{address}", accHandler.DeleteNonCustodialAccount()).Methods(http.MethodDelete)
+
+	// Non-custodial account.
+	adminAuthorizer, err := km.AdminAuthorizer(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nonCustodialAccount := test.NewFlowAccount(t, fc, adminAuthorizer.Address, adminAuthorizer.Key, adminAuthorizer.Signer)
+
+	account := accounts.Account{Address: nonCustodialAccount.Address.Hex()}
+	buf := bytes.NewBuffer(asJson(&account))
+	res := send(router, http.MethodPost, "/", buf)
+	assertStatusCode(t, res, http.StatusCreated)
+	fromJsonBody(res, &account)
+
+	res = send(router, http.MethodGet, fmt.Sprintf("/%s", account.Address), nil)
+	assertStatusCode(t, res, http.StatusOK)
+	fromJsonBody(res, &account)
+
+	if account.Address != flow_helpers.FormatAddress(nonCustodialAccount.Address) {
+		t.Fatalf("read account address doesn't match - expected %q, got %q", flow_helpers.FormatAddress(nonCustodialAccount.Address), account.Address)
+	}
+
+	res = send(router, http.MethodDelete, fmt.Sprintf("/%s", account.Address), nil)
+	assertStatusCode(t, res, http.StatusOK)
+
+	res = send(router, http.MethodGet, fmt.Sprintf("/%s", account.Address), nil)
+	assertStatusCode(t, res, http.StatusNotFound)
+}
+
 func assertStatusCode(t *testing.T, res *http.Response, expected int) {
 	t.Helper()
 	if res.StatusCode != expected {
